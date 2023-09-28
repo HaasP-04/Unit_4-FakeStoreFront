0c11e45448b9f3ae964f2408e2bb04004aa6ac40
HaasP-04 <hasanpringle@gmail.com>
2023-09-27 18:07:01 -0600

    added java card details

--- index.js
+++ index.js
@@ -1,20 +1,78 @@
-let electronics = document.getElementById("electronics")
+let electronics = document.getElementById("electronics");
+let jewerely = document.getElementById("jewerely");
+let mens = document.getElementById("mens");
+let womens = document.getElementById("womens");
+let display = document.getElementById("display");
 
+function displayCards(products) {
+  products.forEach((product) => {
+
+    let card = document.createElement("div");
+    card.className = "card";
+    card.style = "width: 18rem";
+    display.appendChild(card);
+
+    let image = document.createElement("img");
+    image.src = product.image;
+    image.alt = product.title;
+    image.className = "card-img-top";
+    card.appendChild(image);
+    
+    let cardbody = document.createElement("div");
+    cardbody.className = "card-body";
+    card.appendChild(cardbody);
+    
+    let cardtitle = document.createElement("h5");
+    cardtitle.className = "card-title";
+    cardtitle.innerText = "Card Title";
+    cardbody.appendChild(cardtitle);
+    
+    let cardtext = document.createElement("p");
+    cardtext.className = "card-text";
+    cardtext.innerText =
+    "Some quick example text to build on the card title and make up the bulk of the card's content.";
+    cardbody.appendChild(cardtext);
+
+    let listgrouplistgroupflush = document.createComment("ul");
+    ul.className = "list-group lsit-group-flush";
+    card.appendChild(listgrouplistgroupflush);
+
+    let listgroupitem1 = document.createElement("li");
+    li.className = "lsit-group-item";
+    li.innerText = "Description";
+    listgrouplistgroupflush.appendChild(listgroupitem1);
+    
+    let listgroupitem2 = document.createElement("li");
+    li.className = "lsit-group-item";
+    li.innerText = "Price";
+    listgrouplistgroupflush.appendChild(listgroupitem2);
+
+    let cardbody2 = document.createElement("div");
+    cardbody2.className = "card-body2";
+    card.appendChild(cardbody2);
+
+    
+
+  
+
+
+
+    
+  });
+}
 
 // variable that will hold API URL
 async function getfakeStore(endpoint) {
-  const response = await fetch ('https://fakestoreapi.com/products'+ endpoint)
+  const response = await fetch("https://fakestoreapi.com/products" + endpoint);
   let data = await response.json();
 
-  console.log(data);
-
-  // Fix errors in function 
+  // Fix errors in function
   // return data;
-
 
   // console.error('Error:', error);
   // throw error;
 
+  displayCards(data);
 }
 
 window.onload = (event) => {
@@ -22,16 +80,16 @@ window.onload = (event) => {
 };
 
 electronics.addEventListener("click", (e) => {
-  getfakeStore("/category/electronics")
+  getfakeStore("/category/electronics");
 });
-jewelery.addEventListener("click", (e) => {
-  getfakeStore("/category/jewelery")
+jewerely.addEventListener("click", (e) => {
+  getfakeStore("/category/jewerely");
 });
-mensclothing.addEventListener("click", (e) => {
-  getfakeStore("/category/men's clothing")
+mens.addEventListener("click", (e) => {
+  getfakeStore("/category/mens");
 });
-womensclothing.addEventListener("click", (e) => {
-  getfakeStore("/category/women's clothing")
+womens.addEventListener("click", (e) => {
+  getfakeStore("/category/womens");
 });
 // form.addEventListener("submit", (e) => {
 //   e.preventDefault();
